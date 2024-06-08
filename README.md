47c54ccfcae72b98caf44749c115672a0e39ba06
CitizenOneX <17427385+CitizenOneX@users.noreply.github.com>
2024-06-08 18:01:59 +1000

    initial flutter create

---
+++ README.md
@@ -0,0 +1,3 @@
+# frame_flutter_ticker
+
+Subscribes to a Finnhub.io ticker symbol using WebSockets and pushes updates to Frame display
---
+++ android/app/src/main/kotlin/com/example/frame_flutter_ticker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.frame_flutter_ticker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
