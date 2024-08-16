a694dec356bf8e11ec9a71258c975032beeb90d3
CitizenOneX <17427385+CitizenOneX@users.noreply.github.com>
2024-08-16 11:57:01 +1000

    updated simple_frame_app

--- lib/main.dart
+++ lib/main.dart
@@ -33,11 +33,10 @@ class MainAppState extends State<MainApp> with SimpleFrameAppState {
     Logger.root.onRecord.listen((record) {
       debugPrint('${record.level.name}: ${record.time}: ${record.message}');
     });
-
-    currentState = ApplicationState.disconnected;
   }
 
   /// subscribe to a ticker feed for the user's selected ticker using their API token
+  @override
   Future<void> run() async {
     currentState = ApplicationState.running;
     if (mounted) setState(() {});
@@ -63,10 +62,11 @@ class MainAppState extends State<MainApp> with SimpleFrameAppState {
           // only update the frame display if the ticker value has changed
           if (_tickerText != prevTickerText) {
             await FrameHelper.writeText(frame!, _tickerText);
-            await Future.delayed(const Duration(milliseconds: 100));
+            await Future.delayed(const Duration(milliseconds: 150));
             await FrameHelper.show(frame!);
             prevTickerText = _tickerText;
           }
+
           await Future.delayed(const Duration(milliseconds: 2000));
         }
 
@@ -82,6 +82,7 @@ class MainAppState extends State<MainApp> with SimpleFrameAppState {
     if (mounted) setState(() {});
   }
 
+  @override
   Future<void> cancel() async {
     currentState = ApplicationState.ready;
     if (mounted) setState(() {});
@@ -103,44 +104,6 @@ class MainAppState extends State<MainApp> with SimpleFrameAppState {
 
   @override
   Widget build(BuildContext context) {
-    // work out the states of the footer buttons based on the app state
-    List<Widget> pfb = [];
-
-    switch (currentState) {
-      case ApplicationState.disconnected:
-        pfb.add(TextButton(onPressed: scanOrReconnectFrame, child: const Text('Connect')));
-        pfb.add(const TextButton(onPressed: null, child: Text('Start')));
-        pfb.add(const TextButton(onPressed: null, child: Text('Stop')));
-        pfb.add(const TextButton(onPressed: null, child: Text('Disconnect')));
-        break;
-
-      case ApplicationState.initializing:
-      case ApplicationState.scanning:
-      case ApplicationState.connecting:
-      case ApplicationState.running:
-      case ApplicationState.stopping:
-      case ApplicationState.disconnecting:
-        pfb.add(const TextButton(onPressed: null, child: Text('Connect')));
-        pfb.add(const TextButton(onPressed: null, child: Text('Start')));
-        pfb.add(const TextButton(onPressed: null, child: Text('Stop')));
-        pfb.add(const TextButton(onPressed: null, child: Text('Disconnect')));
-        break;
-
-      case ApplicationState.connected:
-        pfb.add(const TextButton(onPressed: null, child: Text('Connect')));
-        pfb.add(TextButton(onPressed: startApplication, child: const Text('Start')));
-        pfb.add(const TextButton(onPressed: null, child: Text('Stop')));
-        pfb.add(TextButton(onPressed: disconnectFrame, child: const Text('Disconnect')));
-        break;
-
-      case ApplicationState.ready:
-        pfb.add(const TextButton(onPressed: null, child: Text('Connect')));
-        pfb.add(const TextButton(onPressed: null, child: Text('Start')));
-        pfb.add(TextButton(onPressed: stopApplication, child: const Text('Stop')));
-        pfb.add(const TextButton(onPressed: null, child: Text('Disconnect')));
-        break;
-    }
-
     return MaterialApp(
       title: 'Frame Flutter Ticker',
       theme: ThemeData.dark(),
@@ -184,12 +147,8 @@ class MainAppState extends State<MainApp> with SimpleFrameAppState {
             ),
           )
         ),
-        floatingActionButton:
-          currentState == ApplicationState.ready ?
-            FloatingActionButton(onPressed: run, child: const Icon(Icons.auto_graph)) :
-          currentState == ApplicationState.running ?
-          FloatingActionButton(onPressed: cancel, child: const Icon(Icons.cancel)) : null,
-        persistentFooterButtons: pfb,
+        floatingActionButton: getFloatingActionButtonWidget(const Icon(Icons.auto_graph), const Icon(Icons.cancel)),
+        persistentFooterButtons: getFooterButtonsWidget(),
       ),
     );
   }
